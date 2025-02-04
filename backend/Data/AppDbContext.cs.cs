aa32596dd250ff0b841a62e5671d2833abd25569
nebrasskz <senkezsenkez@gmail.com>
2025-02-04 14:26:48 +0100

    on a remplie le ficher AppDbCont

--- backend/Data/AppDbContext.cs.cs
+++ backend/Data/AppDbContext.cs.cs
@@ -1,6 +1,21 @@
-﻿namespace backend.Data
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employment> Employments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employment>()
+                .HasOne(e => e.Department)
+                .WithMany()
+                .HasForeignKey(e => e.Department);
+        }
     }
 }
