eb11edc256430082bb71841a8f4350393ab301f3
nebrasskz <senkezsenkez@gmail.com>
2025-02-04 13:55:53 +0100

    Ajoutez des fichiers projet.

---
+++ backend/Models/Employment.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class Employment
+    {
+        public int EmploymentId { get; set; }
+        public string EmploymentName { get; set; }
+        public int DepartmentId { get; set; } // clé étranère
+        public Department Department { get; set; }
+    }
+}
