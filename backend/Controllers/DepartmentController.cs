5e23ba9aa2408ab56e3c62c516c9735e2f7fe88c
nebrasskz <senkezsenkez@gmail.com>
2025-02-04 15:46:25 +0100

    entrain d'écrire le code de departmentcontroller

--- backend/Controllers/DepartmentController.cs
+++ backend/Controllers/DepartmentController.cs
@@ -1,6 +1,43 @@
-﻿namespace backend.Controllers
+﻿using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
 {
-    public class DepartmentController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public DepartmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartments()
+        {
+            return await _context.Departments.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartments(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return department;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Department>> CreateDepartment(Department department)
+        {
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetDepartments), new { id = department.DepartmentId }, department);
+        }
     }
 }
